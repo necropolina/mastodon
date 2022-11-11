9423a9a8c433647c64a2031833bcc94698d29150
Christian Lawson-Perfect <christianperfect@gmail.com>
2022-11-11 13:26:22 +0000

    remove reference to a thing I ended up not including

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -13,7 +13,6 @@ import SpoilerButtonContainer from '../containers/spoiler_button_container';
 import PrivacyDropdownContainer from '../containers/privacy_dropdown_container';
 import EmojiPickerDropdown from '../containers/emoji_picker_dropdown_container';
 import LaTeXDropdown from '../containers/latex_dropdown_container';
-import LaTeXInputHint from '../components/latex_input_hint';
 import PollFormContainer from '../containers/poll_form_container';
 import UploadFormContainer from '../containers/upload_form_container';
 import WarningContainer from '../containers/warning_container';
