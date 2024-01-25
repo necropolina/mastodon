85962a8f1f8ea1c08cb077cfd492f3946266d87e
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-01-24 18:03:42 -0800

    Tests for boosts in public timelines

--- spec/models/public_feed_spec.rb
+++ spec/models/public_feed_spec.rb
@@ -32,6 +32,34 @@ RSpec.describe PublicFeed do
       expect(subject).to_not include(boost.id)
     end
 
+    context 'with with_reblogs option' do
+      subject { described_class.new(nil, with_reblogs: true).get(20).map(&:id) }
+
+      it 'does include boosts' do
+        status = Fabricate(:status)
+        boost = Fabricate(:status, reblog_of_id: status.id)
+
+        expect(subject).to include(status.id)
+        expect(subject).to include(boost.id)
+      end
+
+      it 'only includes the most recent boost' do
+        poster = Fabricate(:account, domain: nil)
+        booster = Fabricate(:account, domain: nil)
+        second_booster = Fabricate(:account, domain: nil)
+
+        status = Fabricate(:status, account: poster)
+        boost = Fabricate(:status, reblog_of_id: status.id, account: poster)
+        second_boost = Fabricate(:status, reblog_of_id: status.id, account: booster)
+        third_boost = Fabricate(:status, reblog_of_id: status.id, account: second_booster)
+
+        expect(subject).to include(status.id)
+        expect(subject).to_not include(boost.id)
+        expect(subject).to_not include(second_boost.id)
+        expect(subject).to include(third_boost.id)
+      end
+    end
+
     it 'filters out silenced accounts' do
       silenced_account = Fabricate(:account, silenced: true)
       status = Fabricate(:status, account: account)
